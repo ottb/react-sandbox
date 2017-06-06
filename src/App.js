371aff9513eae911b713eb956390aab7c4289439
Brian Ott <ottb@ccf.org>
2017-06-06 11:40:22 -0400

    First component use case

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ButtonComponent from './ButtonComponent'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {numClicks: 0};
+    this.buttonClick = this.buttonClick.bind(this);
+  }
+
+  buttonClick() {
+    this.setState((prevState) => {
+      return {numClicks: prevState.numClicks + 1};
+    });
+    // Since state updates may be asynchronous, we can't output the count of clicks here; it may not have updated yet.
+    // We could use a callback function, but official recommendation is to use componentDidUpdate for that logic, 
+    //   and that is most effectively done in the child component, since otherwise we're having to pass back props (message) from the child to the parent.
+    // In this particular case, I would just leave the click function inside the child and let it manage its own click state.
+  }
+
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+    return(
+      <div>
+        <ButtonComponent message="Here's a message from a parent" buttonClick={this.buttonClick.bind(this)} numClicks={this.state.numClicks} />
       </div>
     );
   }
---
+++ src/ButtonComponent.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+class ButtonComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.buttonClick = this.buttonClick.bind(this);
+
+    if (!this.props.message) {
+        this.message = "Default message";
+    } else {
+        this.message = this.props.message;
+    }
+  }
+
+  buttonClick() {
+    this.props.buttonClick(this.props.message);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.props.numClicks > prevProps.numClicks) {
+      var currentdate = new Date(); 
+      var datetime = currentdate.getDate() + "/"
+                  + (currentdate.getMonth()+1)  + "/" 
+                  + currentdate.getFullYear() + " @ "  
+                  + currentdate.getHours() + ":"  
+                  + currentdate.getMinutes() + ":" 
+                  + currentdate.getSeconds();
+      console.log("Button clicked at " + datetime);
+      console.log("Button has been clicked " + this.props.numClicks + " times");
+      console.log("The button message is " + this.message)
+    }
+  }
+
+render() {
+    return (
+        <div>
+            <button onClick={this.buttonClick}>{this.message}</button>
+        </div>
+    );
+  }
+}
+
+export default ButtonComponent;
